3f42337b1bf4b1ae5723d405bf7db16bafad1e2c
lyt3ral <saisabarishmail@gmail.com>
2024-01-24 08:40:43 +0530

    init: t3 & shadcn

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default async function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      <Button>shadcn</Button>
+    </main>
+  );
+}
+
